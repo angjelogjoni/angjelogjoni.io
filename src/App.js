a14ab027f4203c5ab34b42d6bda01cbee418de2d
Angjelo Gjoni <angjelogjoni@Angjelos-MacBook-Pro-2.local>
2022-02-28 14:33:42 -0500

    modified App.js to say {insert your name here}

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          My name is Angjelo Gjoni and I'm going to master React!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/angjelogjoni"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
